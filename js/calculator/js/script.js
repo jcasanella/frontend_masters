17e993456147d95bad4bd2961f8a238eeb8a6ab8
Jordi Casanella <jordikiter@hotmail.es>
2024-09-10 23:08:58 +0100

    Define events for calculator

--- js/calculator/js/script.js
+++ js/calculator/js/script.js
@@ -1,24 +1,27 @@
 const btnOperations = document.querySelectorAll("button.operator-button");
-
-btnOperations.forEach(but => { console.log(but.innerHTML) });
-
 const btnDigits = document.querySelectorAll("button.standard-button:not(.operator-button)");
 const screenElement = document.querySelector("div.calculator-screen");
 
+// Functions to handle the digits
 btnDigits.forEach(but => { 
     const digit = but.innerHTML;
     
-    but.addEventListener('click', function() {
-        const screenValue = screenElement.innerHTML;   
-                
-        if (digit === 'C') { 
-            cleanScreen(screenElement);
-        } else if(digit === '←') {   // Remove last digit
-            removeDigit(screenElement, screenValue);
-        } else {
-            screenElement.innerHTML = screenValue === '0' ?  digit : screenValue + digit;
-        }
-    });
+    switch(digit) {
+        case 'C':
+            but.addEventListener('click', function() {
+                cleanScreen(screenElement);
+            });
+            break;
+        case '←':
+            but.addEventListener('click', function() {
+                removeDigit(screenElement);
+            });
+            break;
+        default:
+            but.addEventListener('click', function() {                
+                addDigit(screenElement, digit);
+            });
+    }
 });
 
 const cleanScreen = (elem) => {
@@ -27,11 +30,57 @@ const cleanScreen = (elem) => {
     }
 };
 
-const removeDigit = (elem, screenValue) => {
+const removeDigit = (elem) => {
     if (elem) {
+        const screenValue = elem.innerHTML; 
         if (screenValue !== '0') {
             const value = screenValue.substring(0, screenValue.length - 1);
             elem.innerHTML = value.length === 0 ? '0' : value;    
         }
     }
 };
+
+const addDigit = (elem, digit) => {
+    if (elem) {
+        const screenValue = elem.innerHTML;   
+        elem.innerHTML = screenValue === '0' ?  digit : screenValue + digit;
+    }
+};
+
+// Functions to handle the operations
+btnOperations.forEach(but => {
+    const operator = but.innerHTML;
+    // const screenValue = screenElement.innerHTML; 
+
+    switch(operator) {
+        case '÷':
+            but.addEventListener('click', function() {
+                console.log('Divide');
+            });
+            break;
+
+        case 'x':
+            but.addEventListener('click', function() {
+                console.log('Product');
+            });
+            break;
+
+        case '-':
+            but.addEventListener('click', function() {
+                console.log('Substract');
+            });
+            break;
+
+        case '+':
+            but.addEventListener('click', function() {
+                console.log('Add');
+            });
+            break;
+    
+        case '=':
+            but.addEventListener('click', function() {
+                console.log('Assign');
+            });
+            break;            
+    }
+});
