4136ca6cd10ba5b107be862e3dc75a6f8fd7f078
Jordi Casanella <jordikiter@hotmail.es>
2024-09-28 00:58:05 +0100

    Add animation

--- js/word_masters/js/script.js
+++ js/word_masters/js/script.js
@@ -1,9 +1,10 @@
-function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
-}
-
-function enterCharacter(elem, event) {
+// Get a character and validates if it's valid
+const enterCharacter = (elem, event) => {
     console.log(`Event: key ${event.key}`);
+
+    const isLetter = (letter) => {
+        return /^[a-zA-Z]$/.test(letter);
+    };
 
     const charac = event.key;
     if(charac === 'Backspace') {
@@ -13,11 +14,30 @@ function enterCharacter(elem, event) {
     } else {
         console.log(`No valid charac`);
     }    
-}
+};
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.setAttribute('tabindex', '0');
+// Initialize cells
+const initializeCells = () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.setAttribute('tabindex', '0');
+        cell.addEventListener("keydown", (evt) => enterCharacter(cell, evt));
+    });
+};
 
-    cell.addEventListener("keydown", (evt) => enterCharacter(cell, evt));
+// Delay function
+const delay = (time) =>  new Promise(resolve => setTimeout(resolve, time));
+
+// Hide Spiral After 5 Seconds
+const hideSpiralAfterTimeout = () => {
+    setTimeout(() => {
+        const elemSpiral = document.querySelector(".wrapper_spiral");
+        elemSpiral.style.display = 'none';
+    }, 5000);
+};
+
+// Initialize all on page load
+document.addEventListener('DOMContentLoaded', () => {
+    initializeCells();
+    hideSpiralAfterTimeout();
 });
