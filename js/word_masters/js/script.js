0effd24dbaf423a2381a8e3e2024a67cf879b84f
Jordi Casanella <jordikiter@hotmail.es>
2024-09-29 01:46:57 +0100

    check if row is valid

--- js/word_masters/js/script.js
+++ js/word_masters/js/script.js
@@ -1,10 +1,30 @@
+// global variables
+let attempts = 0;
+
+// Check if the cell focus is in a valid row
+const isValidRow = (elem) => {
+    const validRow = `r${attempts + 1}`;
+    const classesElement = elem.className.split(' ');
+    if (!classesElement.find((c) => c === validRow)) {
+        console.log(`Class: ${elem.className}`);
+        return false;
+    }
+
+    return true;
+};
+
+const isLetter = (letter) => {
+    return /^[a-zA-Z]$/.test(letter);
+};
+
 // Get a character and validates if it's valid
 const enterCharacter = (elem, event) => {
     console.log(`Event: key ${event.key}`);
 
-    const isLetter = (letter) => {
-        return /^[a-zA-Z]$/.test(letter);
-    };
+    // If not valid row, dont allow to edit
+    if (!isValidRow(elem)) {
+        return;
+    }
 
     const charac = event.key;
     if(charac === 'Backspace') {
@@ -12,7 +32,7 @@ const enterCharacter = (elem, event) => {
     } else if(isLetter(charac)) {
         elem.innerText = event.key;
     } else {
-        console.log(`No valid charac`);
+        event.preventDefault();
     }    
 };
 
@@ -36,8 +56,28 @@ const hideSpiralAfterTimeout = () => {
     }, 5000);
 };
 
+// Get word of the day
+const  getWordOfDay = async () => {
+    const url = 'https://words.dev-apis.com/word-of-the-day';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(`Response: ${JSON.stringify(json)}`);
+
+        const { word } = json;
+        console.log(`Word: ${word}`);
+    } catch(error) {
+        console.error(error.message);
+    }
+};
+
 // Initialize all on page load
 document.addEventListener('DOMContentLoaded', () => {
     initializeCells();
     hideSpiralAfterTimeout();
+    getWordOfDay();
 });
